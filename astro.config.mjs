d4f82f504bf367ec0e92bdb1bbca617d2f8ba37b
liancasi <lacaro@ut.edu.co>
2024-12-27 11:18:14 -0500

    ooooK

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://liancasi.github.io',
   base: '/Portafolio',
+  integrations: [tailwind()],
+  outDir: './dist',
   build: {
     assets: 'assets'
   }
