2fe5ad9a19d3ade6835ce3990fcfe348e1f6bb80
liancasi <lacaro@ut.edu.co>
2024-12-27 02:22:19 -0500

    Corrigiendo portafolio4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://liancasi.github.io', 
-  base: '/Portafolio/',  
-  outDir: './docs',
+  site: 'https://liancasi.github.io',
+  base: '/Portafolio/',
+  outDir: './dist', 
 });
+
