576956dfe6d31af1defdd87c41666042e362a08e
liancasi <lacaro@ut.edu.co>
2024-12-27 02:27:14 -0500

    Update deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://liancasi.github.io',
-  base: '/Portafolio/',
-  outDir: './dist', 
+  base: '/Portafolio',
+  outDir: './dist',
 });
-
